bc74df7a9fa3c76b0320b7fecc1818d110d201d0
barahuang <bara.huang@gmail.com>
2017-10-29 16:05:03 -0700

    Updated server

--- app/server/index.js
+++ app/server/index.js
@@ -3,32 +3,30 @@ var cors = require('cors');
 var express = require('express');
 var app = express();
 
-var parser = require('rss-parser');
-const url = 'https://blogs.esri.com/esri/arcgis/feed';
-const options = {
-  customFields: {
-    feed: [['generator', 'author'], ['content:encoded', 'content']],
-    item: [['dc:creator', 'author'], ['content:encoded', 'contentHtml']]
-  }
-};
-let feed = null;
-
-parser.parseURL(url, options, (err, parsed) => {
-  feed = parsed.feed;
-});
-
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cors());
-
+app.use(express.static('../client/build'));
+//app.get('/*', (req, res) => {
+//  res.sendFile(path.join(__dirname, '../client/build','index.html'));});
 app.set('port', process.env.PORT || 3001);
 app.get('/feed', (req, res) => {
   console.log(`[GET:/feed] ${new Date().toLocaleString()}`);
-  res.json({
-    feed
+  var parser = require('rss-parser');
+  const url = 'https://blogs.esri.com/esri/arcgis/feed';
+  const options = {
+    customFields: {
+      feed: [['generator', 'author'], ['content:encoded', 'content']],
+      item: [['dc:creator', 'author'], ['content:encoded', 'contentHtml']]
+    }
+  };
+  parser.parseURL(url, options, (err, parsed) => {
+    res.json({
+      feed: parsed.feed
+    });
   });
 });
 
-app.get('/', (req, res) => {
+app.get('/check', (req, res) => {
   console.log(`[GET:/] ${new Date().toLocaleString()}`);
   res.send('Server is working.');
 });
